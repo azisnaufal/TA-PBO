4cdc3d7828a963ad77480433da359e900dc74420
Farhan Rafly Fahrezi <raflyfahr15@gmail.com>
2020-01-13 11:41:00 +0700

    add anggota.java

--- src/application/anggota/Anggota.java
+++ src/application/anggota/Anggota.java
@@ -10,5 +10,60 @@ package application.anggota;
  * @author Farhan Rafly Fahrezi
  */
 public class Anggota {
+    private String id_anggota;
+    private String no_KTP;
+    private String nama_lengkap;
+    private String alamat;
+    private String ttl;
+    private String nomor_telepon;
+
+    public String getId_anggota() {
+        return id_anggota;
+    }
+
+    public void setId_anggota(String id_anggota) {
+        this.id_anggota = id_anggota;
+    }
+
+    public String getNo_KTP() {
+        return no_KTP;
+    }
+
+    public void setNo_KTP(String no_KTP) {
+        this.no_KTP = no_KTP;
+    }
+
+    public String getNama_lengkap() {
+        return nama_lengkap;
+    }
+
+    public void setNama_lengkap(String nama_lengkap) {
+        this.nama_lengkap = nama_lengkap;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getTtl() {
+        return ttl;
+    }
+
+    public void setTtl(String ttl) {
+        this.ttl = ttl;
+    }
+
+    public String getNomor_telepon() {
+        return nomor_telepon;
+    }
+
+    public void setNomor_telepon(String nomor_telepon) {
+        this.nomor_telepon = nomor_telepon;
+    }
+    
     
 }
