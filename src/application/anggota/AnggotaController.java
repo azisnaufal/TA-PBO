1c7a3ff2930d937df3f0a42c2fe3ae1eebee12d9
Farhan Rafly Fahrezi <raflyfahr15@gmail.com>
2020-01-15 10:00:44 +0700

    success upload data anggota

--- src/application/anggota/AnggotaController.java
+++ src/application/anggota/AnggotaController.java
@@ -5,7 +5,7 @@
  */
 package application.anggota;
 
-import application.mahasiswa.MahasiswaController;
+import java.util.List;
 
 /**
  *
@@ -19,5 +19,55 @@ public class AnggotaController {
     private AnggotaController() {
         this.view = new AnggotaView();
         this.repos = AnggotaRepository.getInstance();
+    }
+    
+    public static AnggotaController getInstance() {
+        if (instance == null) {
+            instance = new AnggotaController();
+        }
+        
+        return instance;
+    }
+    
+    public void index() {
+        boolean loop = true;
+        while (loop) {
+            int menu = view.menu();
+            switch (menu) {
+                case 1:
+                        this.tambah();
+                    break;
+                default: {
+                    System.out.println("Pilihan tidak tersedia");
+                    break;
+                }
+            }
+        }
+    }
+    
+    public void tambah() {
+        Anggota anggota = view.form();
+        view.alertLoading();
+        
+//        List<Anggota> anggotas = repos.get(anggota.getNo_KTP());
+//        
+//        if (anggotas.size() > 0) {
+//            view.alertKTPExist();
+//        }
+//        else {
+//            boolean saved = repos.insert(anggota);
+//            if (saved) {
+//                view.alertDataSaved();
+//            } else {
+//                view.alertDataNotSaved();
+//            }
+//        }
+        
+        boolean saved = repos.insert(anggota);
+            if (saved) {
+                view.alertDataSaved();
+            } else {
+                view.alertDataNotSaved();
+            }
     }
 }
