422c4b5767fbec802f77289f484de5d6cdf489ce
Farhan Rafly Fahrezi <raflyfahr15@gmail.com>
2020-01-15 10:01:04 +0700

    success upload data anggota

--- src/application/anggota/AnggotaRepository.java
+++ src/application/anggota/AnggotaRepository.java
@@ -37,10 +37,36 @@ public class AnggotaRepository {
     public List<Anggota> get(String params) {
         List<Anggota> anggotas = new ArrayList<>();
         
-        String sql = "";
+        String sql = "INSERT INTO Anggota (no_KTP, nama_lengkap, alamat, ttl, nomor_telepon)";
         
-        
+        try {
+            
+        } catch (Exception e) {
+            
+        }
         
         return anggotas;
+    }
+    
+    public boolean insert(Anggota anggota) {
+        boolean success = false;
+        String sql = "INSERT INTO Anggota (id_anggota, no_KTP, nama_lengkap, alamat, ttl, nomor_telepon) value(?,?,?,?,?,?)";
+        Connection con = db.getConnection();
+        
+        try {
+            PreparedStatement preparedStatement = con.prepareStatement(sql);
+            preparedStatement.setString(1, anggota.getId_anggota());
+            preparedStatement.setString(2, anggota.getNo_KTP());
+            preparedStatement.setString(3, anggota.getNama_lengkap());
+            preparedStatement.setString(4, anggota.getAlamat());
+            preparedStatement.setString(5, anggota.getTtl());
+            preparedStatement.setString(6, anggota.getNomor_telepon());
+            
+            preparedStatement.execute();
+            success = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return success;
     }
 }
